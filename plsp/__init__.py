aa625cdf38d8b71be8d458f7c2676a4ab5261f68
gamasenninn <gamasenninn@yahoo.co.jp>
2023-03-27 02:14:13 +0900

    最小化

--- plsp/__init__.py
+++ plsp/__init__.py
@@ -1,8 +1,8 @@
 from .plsp import main
-from .repl import repl_main
-from .file_runner import run_file
-from .lparser import parse
-from .evaluator import evaluate, initial_env
-from .utils import schemestr
+#from .repl import repl_main
+#from .file_runner import run_file
+#from .lparser import parse
+#from .evaluator import evaluate, initial_env
+#from .utils import schemestr
 
-__all__ = ['main','repl_main','run_file','parse','evaluate','initial_env','schemestr'] 
+#__all__ = ['main','repl_main','run_file','parse','evaluate','initial_env','schemestr'] 
