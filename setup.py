c58bab55d48b7c9f0c09219ba865b9e01754dd25
gamasenninn <gamasenninn@yahoo.co.jp>
2023-03-26 14:42:34 +0900

    パッケージ化

---
+++ setup.py
@@ -0,0 +1,16 @@
+# setup.py
+from setuptools import setup, find_packages
+
+setup(
+    name='plsp',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=[
+        # 依存関係のあるパッケージをここに記述します。
+    ],
+    entry_points={
+        'console_scripts': [
+            'plsp = plsp:main',
+        ],
+    },
+)
